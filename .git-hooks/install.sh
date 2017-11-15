935d7716229f0eca1330dd75f198b56da8c4de6d
freebsdfrau <freebsdfrau@shxd.cx>
2017-11-15 13:09:19 -0800

    Improvements to git-hook installation script
    Add -f option to force all steps to be performed
    Add a usage statement, shown if given -h
    Don't create links if they already exist
    Check the user's name/email configuration

--- .git-hooks/install.sh
+++ .git-hooks/install.sh
@@ -3,11 +3,18 @@
 #
 # $Title: Script to enable client side hooks $
 # $Copyright: 2017 Devin Teske. All rights reserved. $
-# $FrauBSD: .git-hooks/install.sh 2017-10-03 15:42:32 -0700 freebsdfrau $
+# $FrauBSD: .git-hooks/install.sh 2017-11-15 13:09:19 -0800 freebsdfrau $
 #
 ############################################################ GLOBALS
 
+pgm="${0##*/}" # Program basename
 progdir="${0%/*}" # Program directory
+
+#
+# Global exit status
+#
+SUCCESS=0
+FAILURE=1
 
 #
 # Stdout processing
@@ -24,7 +31,28 @@ ANSI_BLD_OFF="${CONSOLE:+$ESC[22m}"
 ANSI_GRN_ON="${CONSOLE:+$ESC[32m}"
 ANSI_FGC_OFF="${CONSOLE:+$ESC[39m}"
 
+#
+# Command-line options
+#
+FORCE=	# -f
+
 ############################################################ FUNCTIONS
+
+usage()
+{
+	local optfmt="\t%10s %s\n"
+	exec >&2
+	printf "Usage: %s [OPTIONS]\n" "$pgm"
+	printf "OPTIONS:\n"
+	printf "$optfmt" "-f" \
+		"Force. Do not skip any steps for any reason."
+	exit $FAILURE
+}
+
+have()
+{
+	type "$@" > /dev/null 2>&1
+}
 
 eval2()
 {
@@ -32,9 +60,27 @@ eval2()
 	eval "$@"
 }
 
+eval3()
+{
+	echo "$ANSI_BLD_ON$ANSI_GRN_ON==>$ANSI_FGC_OFF $*$ANSI_BLD_OFF" >&3
+	eval "$@"
+}
+
 ############################################################ MAIN
 
 set -e # Make all errors fatal
+exec 3<&1
+
+#
+# Process command-line options
+#
+while getopts f flag; do
+	case "$flag" in
+	f) FORCE=1 ;;
+	*) usage # NOTREACHED
+	esac
+done
+shift $(( $OPTIND - 1 ))
 
 #
 # Information
@@ -44,6 +90,7 @@ eval2 : "progdir='$progdir'"
 #
 # Make symlinks in .git directory
 #
+linkdir="$progdir/../.git/hooks"
 for file in $( find "$progdir" \
 	-type f -and \
 	-not -name '*.sh' \
@@ -52,13 +99,43 @@ for file in $( find "$progdir" \
 	-and -perm +0111 \
 	| sed -e 's#.*/##'
 ); do
-	eval2 ln -sfv ../../.git-hooks/$file \"\$progdir/../.git/hooks\"
+	link="$linkdir/$file"
+	target="../../.git-hooks/$file"
+	if [ ! "$FORCE" ] && have readlink && [ -L "$link" ]; then
+		linktarget=$( readlink "$link" )
+		if [ "$linktarget" = "$target" ]; then
+			echo "\`$link' -> \`$target'"
+			continue
+		fi
+	fi
+	eval2 ln -sfv \"\$target\" \"\$linkdir/\"
 done
+
+#
+# Make sure name/email are set
+#
+name=$( eval3 git config user.name ) || :
+if [ "$FORCE" -o ! "$name" ]; then
+	default="${name:-$( id -nu )}"
+	read -p "User name [$default]: " name
+	git config user.name "${name:-$default}"
+else
+	echo "User name: $name"
+fi
+email=$( eval3 git config user.email ) || :
+if [ "$FORCE" -o ! "$email" ]; then
+	default="${email:-$( id -nu )@$( hostname )}"
+	read -p "User email [$default]: " email
+	git config user.email "${email:-$default}"
+else
+	echo "User email: $email"
+fi
 
 #
 # Done
 #
 eval2 : SUCCESS
+exit $SUCCESS
 
 ################################################################################
 # END
