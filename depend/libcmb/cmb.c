14d7c2e85d03c597202c088d10a58819f473e039
freebsdfrau <freebsdfrau@shxd.cx>
2018-03-22 16:52:34 -0700

    Import initial cmb(3) work

---
+++ depend/libcmb/cmb.c
@@ -0,0 +1,244 @@
+/*-
+ * Copyright (c) 2002-2018 Devin Teske <dteske@FreeBSD.org>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#include <sys/cdefs.h>
+#ifdef __FBSDID
+__FBSDID("$FrauBSD: depend/libcmb/cmb.c 2018-03-22 16:52:34 -0700 freebsdfrau $");
+__FBSDID("$FreeBSD$");
+#endif
+
+#include <err.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "cmb.h"
+
+/*
+ * Takes pointer to `struct cmb_config' options and array of `char *' items.
+ * Calculates combinations according to options and either prints combinations
+ * to stdout (the default) or runs the `action' function passed-in as a member
+ * of the `config' argument.
+ */
+int cmb(struct cmb_config *config, int nitems, char *items[])
+{
+	int nextset = 1;
+	int retval = 0;
+	uint curset;
+	uint setinit = 0;
+	uint setdone = nitems;
+	char **curitems;
+	uint *setnums;
+	uint *setnums_backend;
+	int (*action)(int nitems, char *items[]) = cmb_items_print;
+
+	if (config != NULL) {
+		if (config->action != NULL) action = config->action;
+		if (config->delimiter != NULL)
+			cmb_print_delimiter = config->delimiter;
+		if (config->range_min == 0 && config->range_max == 0) {
+			setinit = 1;
+			setdone = nitems;
+		} else {
+			setinit = config->range_min;
+			setdone = config->range_max;
+		}
+	}
+
+	/* Adjust values to be non-zero (mathematical constraint) */
+	if (setinit == 0) setinit = 1;
+	if (setdone == 0) setdone = 1;
+
+	/* Enforce limits so we don't run over bounds */
+	if (setinit > setdone) setdone = setinit;
+	if (setinit > nitems) setinit = nitems;
+	if (setdone > nitems) setdone = nitems;
+
+	/* Set the direction of flow (incrementing vs. decrementing) */
+	if (setinit > setdone) nextset = -1;
+
+	/*
+	 * Allocate memory
+	 */
+	if ((curitems = (char **)malloc(sizeof(char *) * setdone)) == NULL)
+		errx(EXIT_FAILURE, "Out of memory?!");
+	if ((setnums = (uint *)malloc(sizeof(uint) * setdone)) == NULL)
+		errx(EXIT_FAILURE, "Out of memory?!");
+	if ((setnums_backend = (uint *)malloc(sizeof(uint) * setdone)) == NULL)
+		errx(EXIT_FAILURE, "Out of memory?!");
+
+	/*
+	 * Loop over each `set' in the configured direction until we are done
+	 * NB: Each `set' can represent a single item or multiple items
+	 */
+	for (curset = setinit; curset <= setdone; curset += nextset) {
+		uint combo;
+		uint i;
+		uint n;
+		uint ncombos;
+		uint nsubsets;
+		uint setpos;
+		uint setpos_backend;
+		uint64_t d;
+		uint64_t z;
+
+		/*
+		 * Calculate number of subsets, based on the number of items in
+		 * the current set we're working on.
+		 */
+		nsubsets = nitems - curset + 1;
+
+		/*
+		 * Calculate number of combinations based on number of subsets
+		 */
+		z = d = 1;
+		for (n = 0; n < curset; n++) {
+			z *= nsubsets + n;
+			d *= n + 1;
+		}
+		ncombos = z / d;
+
+		/*
+		 * Fill array with the initial positional arguments
+		 */
+		for (n = 0; n < curset; n++) curitems[n] = items[n];
+
+		/*
+		 * Produce results with the first set of items
+		 */
+		if ((retval = action(curset, curitems)) != 0) goto cmb_return;
+
+		/*
+		 * Prefill two arrays used for matrix calculations.
+		 *
+		 * The first array (setnums) is a linear sequence starting at
+		 * one (1) and ending at N (where N is the same integer as the
+		 * current set we're operating on). For example, if we are
+		 * currently on a set-of-2, setnums is 1, 2.
+		 *
+		 * The second array (setnums_backend) is a linear sequence
+		 * starting at nitems-N and ending at nitems (again, N is the
+		 * same integer as the current set we are operating on; nitems
+		 * is the total number of items). For example, if we are
+		 * operating on a set-of-2, and nitems is 8, setnums_backend is
+		 * set to 7, 8.
+		 */
+		i = 0;
+		for (n = 0; n < curset; n++) setnums[n] = n;
+		for (n = curset; n > 0; n--) setnums_backend[i++] = nitems - n;
+
+		/*
+		 * Process remaining self-similar combinations in the set
+		 */
+		for (combo = 1; combo < ncombos; combo++) {
+			uint seed;
+			uint setnums_last;
+
+			/*
+			 * Using self-similarity (matrix) theorem, determine
+			 * (by comparing the [sliding] setnums to the stored
+			 * setnums_backend) the number of arguments that remain
+			 * available for shifting into a new setnums value
+			 * (for later mapping into curitems).
+			 *
+			 * In essence, determine when setnums has slid into
+			 * setnums_backend in which case we can mathematically
+			 * use the last item to find the next-new item.
+			 */
+			for (n = curset; n > 0; n--) {
+				setpos = setnums[n - 1];
+				setpos_backend = setnums_backend[n - 1];
+				/*
+				 * If setpos is equal to or greater than
+				 * setpos_backend then we keep iterating over
+				 * the current set's list of argument positions
+				 * until otherwise; each time incrementing the
+				 * amount of numbers we must produce from
+				 * formulae rather than stored position.
+				 */
+				setnums_last = n - 1;
+				if (setpos < setpos_backend) break;
+			}
+
+			/*
+			 * The next few stanzas are dedicated to rebuilding the
+			 * setnums array for mapping positional items
+			 * [immediately following] into curitems.
+			 */
+
+			/*
+			 * Get the generator number used to populate unknown
+			 * positions in the matrix (using self-similarity).
+			 */
+			seed = setnums[setnums_last];
+
+			/*
+			 * Use the generator number to populate any position
+			 * numbers that weren't carried over from previous
+			 * combination run -- using self-similarity theorem.
+			 */
+			for (n = setnums_last; n <= curset; n++)
+				setnums[n] = seed + n - setnums_last + 1;
+
+			/*
+			 * Now map the new setnums into values stored in items
+			 */
+			for (n = 0; n < curset; n++)
+				curitems[n] = items[setnums[n]];
+
+			/*
+			 * Produce results with this set of items
+			 */
+			if ((retval = action(curset, curitems)) != 0)
+				goto cmb_return;
+
+		} /* for combo */
+
+	} /* for curset */
+
+cmb_return:
+	free(curitems);
+	free(setnums);
+	free(setnums_backend);
+	return (retval);
+}
+
+int
+cmb_items_print(int nitems, char *items[])
+{
+	int n;
+
+	for (n = 0; n < nitems; n++) {
+		printf("%s", items[n]);
+		if (n < nitems - 1 && cmb_print_delimiter != NULL)
+			printf("%s", cmb_print_delimiter);
+	}
+	printf("\n");
+
+	return (0);
+}
---
+++ depend/libcmb/cmb.h
@@ -0,0 +1,59 @@
+/*-
+ * Copyright (c) 2002-2018 Devin Teske <dteske@FreeBSD.org>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * $FrauBSD: depend/libcmb/cmb.h 2018-03-22 16:52:34 -0700 freebsdfrau $
+ * $FreeBSD$
+ */
+
+#ifndef _CMB_H_
+#define _CMB_H_
+
+#include <sys/types.h>
+
+static char *cmb_print_delimiter = " ";
+
+/*
+ * Anatomy of config option to pass as cmb() config argument
+ */
+struct cmb_config {
+	char	*delimiter;	/* Item separator (default is " ") */
+	uint	range_min;	/* Minimum number of elements in combination */
+	uint	range_max;	/* Maximum number of elements in combination */
+
+	/*
+	 * Function pointer; action to perform for each combination (default is
+	 * cmb_items_print()). If the return from action() is non-zero, cmb()
+	 * will stop calculation. The cmb() return value is the first non-zero
+	 * result from action(), zero otherwise.
+	 */
+	int (*action)(int nitems, char *items[]);
+};
+
+__BEGIN_DECLS
+int	cmb(struct cmb_config *_config, int _nitems, char *_items[]);
+int	cmb_items_print(int _nitems, char *_items[]);
+__END_DECLS
+
+#endif /* !_CMB_H_ */
