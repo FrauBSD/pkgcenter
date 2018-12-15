b6e8d58ba379d89e1cc328d05cec6d05988e09a9
freebsdfrau <freebsdfrau@shxd.cx>
2018-12-15 07:49:44 -0800

    libcmb: Clean up test2.pl

--- depend/libcmb/perl/test2.pl
+++ depend/libcmb/perl/test2.pl
@@ -3,7 +3,13 @@ use strict;
 use warnings;
 use Cmb;
 
-my $cmb = new Cmb { size_min => 2, size_max => 2 };
-my @items = (1..10000);
-our $n = 1;
-$cmb->cmb_callback($#items+1, \@items, sub { print $n++ . "\n" });
+our $total = 0;
+my $choice = 2;
+my $num = 10000;
+
+my @items = (1..$num);
+my $cmb = new Cmb { size_min => $choice, size_max => $choice };
+
+printf "Silently enumerating choose-%s from %u:\n", $choice, $num;
+$cmb->cmb_callback($#items+1, \@items, sub { $total++ });
+print "$total callbacks executed\n";
