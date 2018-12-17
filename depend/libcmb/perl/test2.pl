51de14fa74bca12daa127dfd97f4aec6705c5288
freebsdfrau <freebsdfrau@shxd.cx>
2018-12-16 18:19:08 -0800

    libcmb: Fix print/printf in perl/test2.pl

--- depend/libcmb/perl/test2.pl
+++ depend/libcmb/perl/test2.pl
@@ -10,6 +10,6 @@ my $num = 10000;
 my @items = (1..$num);
 my $cmb = new Cmb { size_min => $choice, size_max => $choice };
 
-printf "Silently enumerating choose-%s from %u:\n", $choice, $num;
+printf "Silently enumerating choose-%u from %u:\n", $choice, $num;
 $cmb->cmb_callback($#items+1, \@items, sub { $total++ });
-print "$total callbacks executed\n";
+printf "%u callbacks executed\n", $total;
