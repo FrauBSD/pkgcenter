d257a04ab4c18ba43b97015c5d01aae2a2654696
freebsdfrau <freebsdfrau@shxd.cx>
2018-12-17 02:53:06 -0800

    libcmb: test2.pl whitespace

--- depend/libcmb/perl/tests/test2.pl
+++ depend/libcmb/perl/tests/test2.pl
@@ -11,5 +11,5 @@ my @items = (1..$num);
 my $cmb = new Cmb { size_min => $choice, size_max => $choice };
 
 printf "Silently enumerating choose-%u from %u:\n", $choice, $num;
-$cmb->cmb_callback($#items+1, \@items, sub { $total++; return 0 });
+$cmb->cmb_callback($#items + 1, \@items, sub { $total++; return 0 });
 printf "%u callbacks executed\n", $total;
