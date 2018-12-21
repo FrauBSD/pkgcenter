24594b4813e6f2899decca561b3e33baa55459aa
freebsdfrau <freebsdfrau@shxd.cx>
2018-12-20 18:11:17 -0800

    libcmb: Bump version for improved python bindings

--- depend/libcmb/cmb.c
+++ depend/libcmb/cmb.c
@@ -25,7 +25,7 @@
 
 #include <sys/cdefs.h>
 #ifdef __FBSDID
-__FBSDID("$FrauBSD: pkgcenter/depend/libcmb/cmb.c 2018-12-17 02:23:26 -0800 freebsdfrau $");
+__FBSDID("$FrauBSD: pkgcenter/depend/libcmb/cmb.c 2018-12-20 18:11:17 -0800 freebsdfrau $");
 __FBSDID("$FreeBSD$");
 #endif
 
@@ -69,8 +69,8 @@ __FBSDID("$FreeBSD$");
 #define CMB_PARSE_FRAGSIZE 512
 #endif
 
-static const char version[] = "libcmb 2.0";
-static const char version_long[] = "$Version: libcmb 2.0 $";
+static const char version[] = "libcmb 2.2";
+static const char version_long[] = "$Version: libcmb 2.2 $";
 
 #if CMB_DEBUG
 __attribute__((__format__ (__printf__, 1, 0)))
