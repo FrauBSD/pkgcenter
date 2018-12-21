8ec4f689711a9bd76ad67e484c07518aabeea996
freebsdfrau <freebsdfrau@shxd.cx>
2018-12-20 17:48:29 -0800

    libcmb: Add test5.py

---
+++ depend/libcmb/python/test5.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+from cmb import *
+
+total = 0
+items = ["a", "b", "c"]
+nitems = len(items)
+config = CMB()
+
+print "Testing non-zero callback return:"
+def afunc(items):
+    global total
+    total += 1
+    return total - 1
+cmb_callback(config, nitems, items, afunc)
+print "%u of %u callbacks executed" % (total, cmb_count(config, nitems))
