be62e342fd1f032f25896203670e0f1f4b2ccb0a
freebsdfrau <freebsdfrau@shxd.cx>
2019-02-17 19:33:24 -0800

    cputools: Whitespace (spaces to tabs)

--- depend/cputools/htt.c
+++ depend/cputools/htt.c
@@ -25,7 +25,7 @@
 
 #include <sys/cdefs.h>
 #ifdef __FBSDID
-__FBSDID("$FrauBSD: //github.com/FrauBSD/pkgcenter/depend/cputools/htt.c 2019-02-17 19:29:04 -0800 freebsdfrau $");
+__FBSDID("$FrauBSD: //github.com/FrauBSD/pkgcenter/depend/cputools/htt.c 2019-02-17 19:33:24 -0800 freebsdfrau $");
 #endif
 
 #include <sys/sysctl.h>
@@ -47,77 +47,79 @@ __FBSDID("$FrauBSD: //github.com/FrauBSD/pkgcenter/depend/cputools/htt.c 2019-02
 int
 main(int argc, char *argv[])
 {
-   int ch, newl, skip;
-   char *p, *ep, *bp;
-   char buf[SEARCH_LEN + 8 + 1];
-   int all = 0, pri = 0;
-   size_t buflen;
-   int bufpos;
+	int ch, newl, skip;
+	char *p, *ep, *bp;
+	char buf[SEARCH_LEN + 8 + 1];
+	int all = 0, pri = 0;
+	size_t buflen;
+	int bufpos;
 
-   /* Running kernel. Use sysctl. */
-   if (sysctlbyname("kern.msgbuf", NULL, &buflen, NULL, 0) == -1)
-      err(1, "sysctl kern.msgbuf");
-   if ((bp = malloc(buflen)) == NULL)
-      errx(1, "malloc failed");
-   if (sysctlbyname("kern.msgbuf", bp, &buflen, NULL, 0) == -1)
-      err(1, "sysctl kern.msgbuf");
+	/* Running kernel. Use sysctl. */
+	if (sysctlbyname("kern.msgbuf", NULL, &buflen, NULL, 0) == -1)
+		err(1, "sysctl kern.msgbuf");
+	if ((bp = malloc(buflen)) == NULL)
+		errx(1, "malloc failed");
+	if (sysctlbyname("kern.msgbuf", bp, &buflen, NULL, 0) == -1)
+		err(1, "sysctl kern.msgbuf");
 
-   memset(buf, 0, sizeof(buf));
-   bufpos = 0;
+	memset(buf, 0, sizeof(buf));
+	bufpos = 0;
 
-   /*
-    * The message buffer is circular.  If the buffer has wrapped, the
-    * write pointer points to the oldest data.  Otherwise, the write
-    * pointer points to \0's following the data.  Read the entire
-    * buffer starting at the write pointer and ignore nulls so that
-    * we effectively start at the oldest data.
-    */
-   p = bp;
-   ep = bp + buflen;
-   newl = skip = 0;
-   do {
-      if (p == bp + buflen)
-         p = bp;
-      ch = *p;
-      /* Skip "\n<.*>" syslog sequences. */
-      if (skip) {
-         if (ch == '\n') {
-            skip = 0;
-            newl = 1;
-         } if (ch == '>') {
-            if (LOG_FAC(pri) == LOG_KERN || all)
-               newl = skip = 0;
-         } else if (ch >= '0' && ch <= '9') {
-            pri *= 10;
-            pri += ch - '0';
-         }
-         continue;
-      }
-      if (newl && ch == '<') {
-         pri = 0;
-         skip = 1;
-         continue;
-      }
-      if (ch == '\0')
-         continue;
-      newl = ch == '\n';
-      if (ch == '\n') {
-         memset(buf, 0, sizeof(buf));
-         bufpos = 0;
-      } else if (bufpos < SEARCH_LEN + 8) {
-         buf[bufpos++] = ch;
-         if ((bufpos == SEARCH_LEN + 8) && !strncmp(buf, SEARCH, SEARCH_LEN)) {
-            unsigned long features;
-            features = strtoul((char *)(buf + SEARCH_LEN),0,16);
-            if (features & HTT_FLAG)
-               printf("HyperThreading Status: YES\n");
-            else
-               printf("HyperThreading Status: NO\n");
-            return(EXIT_SUCCESS);
-         }
-      }
-   } while (++p != ep);
+	/*
+	 * The message buffer is circular.  If the buffer has wrapped, the
+	 * write pointer points to the oldest data.  Otherwise, the write
+	 * pointer points to \0's following the data.  Read the entire
+	 * buffer starting at the write pointer and ignore nulls so that
+	 * we effectively start at the oldest data.
+	 */
+	p = bp;
+	ep = bp + buflen;
+	newl = skip = 0;
+	do {
+		if (p == bp + buflen)
+			p = bp;
+		ch = *p;
+		/* Skip "\n<.*>" syslog sequences. */
+		if (skip) {
+			if (ch == '\n') {
+				skip = 0;
+				newl = 1;
+			} if (ch == '>') {
+				if (LOG_FAC(pri) == LOG_KERN || all)
+					newl = skip = 0;
+			} else if (ch >= '0' && ch <= '9') {
+				pri *= 10;
+				pri += ch - '0';
+			}
+			continue;
+		}
+		if (newl && ch == '<') {
+			pri = 0;
+			skip = 1;
+			continue;
+		}
+		if (ch == '\0')
+			continue;
+		newl = ch == '\n';
+		if (ch == '\n') {
+			memset(buf, 0, sizeof(buf));
+			bufpos = 0;
+		} else if (bufpos < SEARCH_LEN + 8) {
+			buf[bufpos++] = ch;
+			if ((bufpos == SEARCH_LEN + 8) && !strncmp(buf, SEARCH,
+			    SEARCH_LEN)) {
+				unsigned long features;
+				features = strtoul((char *)(buf + SEARCH_LEN),
+				    0, 16);
+				if (features & HTT_FLAG)
+					printf("HyperThreading Status: YES\n");
+				else
+					printf("HyperThreading Status: NO\n");
+				return (EXIT_SUCCESS);
+			}
+		}
+	} while (++p != ep);
 
-   printf("HyperThreading Status: ERROR\n");
-   return(EXIT_FAILURE);
+	printf("HyperThreading Status: ERROR\n");
+	return (EXIT_FAILURE);
 }
