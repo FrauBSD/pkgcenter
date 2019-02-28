631013bce0a47eaa89b3dbbc221538c35517840c
freebsdfrau <freebsdfrau@shxd.cx>
2019-02-27 17:35:21 -0800

    cmb: Make `-X add' work without `-o'

--- depend/cmb/cmb.c
+++ depend/cmb/cmb.c
@@ -25,7 +25,7 @@
 
 #include <sys/cdefs.h>
 #ifdef __FBSDID
-__FBSDID("$FrauBSD: //github.com/FrauBSD/pkgcenter/depend/cmb/cmb.c 2019-02-27 17:31:38 -0800 freebsdfrau $");
+__FBSDID("$FrauBSD: //github.com/FrauBSD/pkgcenter/depend/cmb/cmb.c 2019-02-27 17:35:21 -0800 freebsdfrau $");
 __FBSDID("$FreeBSD$");
 #endif
 
@@ -67,7 +67,7 @@ __FBSDID("$FreeBSD$");
 #define UINT_MAX 0xFFFFFFFF
 #endif
 
-static char version[] = "$Version: 3.0-alpha-2 $";
+static char version[] = "$Version: 3.0-alpha-3 $";
 
 /* Environment */
 static char *pgm; /* set to argv[0] by main() */
@@ -825,10 +825,30 @@ static int
 cmb_add_bn(struct cmb_config *config, BIGNUM *seq, uint32_t nitems,
     char *items[])
 {
-	(void)config;
-	(void)seq;
-	(void)nitems;
-	(void)items;
+	uint8_t show_numbers = FALSE;
+	uint32_t n;
+	char *seq_str;
+	long double ld;
+	long double total = 0;
+
+	if (config != NULL)
+		show_numbers = config->show_numbers;
+	if (show_numbers) {
+		seq_str = BN_bn2dec(seq);
+		printf("%s ", seq_str);
+#ifdef HAVE_OPENSSL_CRYPTO_H
+		OPENSSL_free(seq_str);
+#endif
+	}
+	for (n = 0; n < nitems; n++) {
+		memcpy(&ld, items[n], sizeof(long double));
+		printf("%.*Lf", cmb_transform_precision, ld);
+		total += ld;
+		if (n < nitems - 1)
+			printf(" + ");
+	}
+	printf(" = %.*Lf", cmb_transform_precision, total);
+	printf("\n");
 	return (0);
 }
 
