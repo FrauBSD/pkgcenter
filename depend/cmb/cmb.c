e984a3e7511ffa2962fc74beddc8fd63d44fc00b
freebsdfrau <freebsdfrau@shxd.cx>
2019-03-29 11:27:29 -0700

    cmb: Update manual to sync with change to -f

--- depend/cmb/cmb.c
+++ depend/cmb/cmb.c
@@ -25,7 +25,7 @@
 
 #include <sys/cdefs.h>
 #ifdef __FBSDID
-__FBSDID("$FrauBSD: pkgcenter/depend/cmb/cmb.c 2019-03-28 20:21:47 -0700 freebsdfrau $");
+__FBSDID("$FrauBSD: pkgcenter/depend/cmb/cmb.c 2019-03-29 11:27:29 -0700 freebsdfrau $");
 __FBSDID("$FreeBSD$");
 #endif
 
@@ -46,7 +46,7 @@ __FBSDID("$FreeBSD$");
 #define UINT_MAX 0xFFFFFFFF
 #endif
 
-static char version[] = "$Version: 3.4-beta-1 $";
+static char version[] = "$Version: 3.4-beta-2 $";
 
 /* Environment */
 static char *pgm; /* set to argv[0] by main() */
