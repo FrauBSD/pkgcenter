7bf371817e207cf8d72cb1174357587c9157f32d
freebsdfrau <freebsdfrau@shxd.cx>
2019-03-29 11:39:23 -0700

    cmb: Update HTML manual initial FreeBSD version reference

--- depend/cmb/cmb.c
+++ depend/cmb/cmb.c
@@ -25,7 +25,7 @@
 
 #include <sys/cdefs.h>
 #ifdef __FBSDID
-__FBSDID("$FrauBSD: pkgcenter/depend/cmb/cmb.c 2019-03-29 11:37:45 -0700 freebsdfrau $");
+__FBSDID("$FrauBSD: pkgcenter/depend/cmb/cmb.c 2019-03-29 11:39:23 -0700 freebsdfrau $");
 __FBSDID("$FreeBSD$");
 #endif
 
@@ -46,7 +46,7 @@ __FBSDID("$FreeBSD$");
 #define UINT_MAX 0xFFFFFFFF
 #endif
 
-static char version[] = "$Version: 3.4.2 $";
+static char version[] = "$Version: 3.4.3 $";
 
 /* Environment */
 static char *pgm; /* set to argv[0] by main() */
