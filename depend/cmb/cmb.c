0aa00f8fdaaeceb8d27be0c02741dbf7f6fe1546
freebsdfrau <freebsdfrau@shxd.cx>
2019-03-30 13:22:03 -0700

    cmb: Fix missing options in HTML manual (fault of man2html)

--- depend/cmb/cmb.c
+++ depend/cmb/cmb.c
@@ -25,7 +25,7 @@
 
 #include <sys/cdefs.h>
 #ifdef __FBSDID
-__FBSDID("$FrauBSD: pkgcenter/depend/cmb/cmb.c 2019-03-30 13:14:24 -0700 freebsdfrau $");
+__FBSDID("$FrauBSD: pkgcenter/depend/cmb/cmb.c 2019-03-30 13:22:03 -0700 freebsdfrau $");
 __FBSDID("$FreeBSD$");
 #endif
 
@@ -46,7 +46,7 @@ __FBSDID("$FreeBSD$");
 #define UINT_MAX 0xFFFFFFFF
 #endif
 
-static char version[] = "$Version: 3.7.2 $";
+static char version[] = "$Version: 3.7.3 $";
 
 /* Environment */
 static char *pgm; /* set to argv[0] by main() */
