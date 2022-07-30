9bc5a7b4ea5cbb79fab288faaf232331e5423645
Faraz Vahedi <kfv@kfv.io>
2022-07-30 16:26:36 +0430

    Revamp put_config_eof and remove unnecessary references

--- depend/libfigput/figput.c
+++ depend/libfigput/figput.c
@@ -40,35 +40,7 @@ __FBSDID("$FreeBSD$");
 #include <string_m.h>
 #include <unistd.h>
 
-#if 0 /* REFERENCE MATERIAL */
-#include <err.h>
-#include <limits.h>
-#include <figpar.h>
-#include <stdbool.h>
-#include <stdint.h>
-#include <sysexits.h>
-#endif
-
 #include "figput.h"
-#if 0 /* REFERENCE MATERIAL */
-#include "parser.h"
-#include "util.h"
-#endif
-
-#if 0 /* REFERENCE MATERIAL */
-#define STR_BUFSIZE 255
-#define defcheck(type) do {                               \
-	if (nodef                 &&                      \
-	    options[i].value.type &&                      \
-	    options[i].value.type == option.value.type)   \
-		errx(EX_USAGE, "value == default value"); \
-} while (0)
-#endif
-
-#if 0 /* REFERENCE MATERIAL */
-extern uint8_t fline;
-extern uint8_t found;
-#endif
 
 /*
  * Search for config option (struct figput_config) in the array of config
@@ -76,8 +48,8 @@ extern uint8_t found;
  * parameter. On success, returns 1, otherwise 0.
  */
 int
-set_config_option(struct figput_config options[], const char *directive,
-    union figput_cfgvalue *value)
+set_config_option(struct figput_config options[static 1],
+    const char *directive, union figput_cfgvalue *value)
 {
 	uint32_t n;
 	struct figput_config *found = NULL;
@@ -102,7 +74,7 @@ set_config_option(struct figput_config options[], const char *directive,
 }
 
 int
-put_config(struct figput_config options[], const char *path,
+put_config(struct figput_config options[static 1], const char *path,
     uint16_t processing_options, uint16_t put_options)
 {
 	uint8_t backup;
@@ -157,15 +129,16 @@ put_config(struct figput_config options[], const char *path,
 		return (-1);
 
 	/* Reading options from processing_options */
-	bequals = (processing_options & FIGPAR_BREAK_ON_EQUALS) == 0 ? 0 : 1;
+	bequals =
+            (processing_options & FIGPAR_BREAK_ON_EQUALS)    == 0 ? 0 : 1;
 	bsemicolon =
-	    (processing_options & FIGPAR_BREAK_ON_EQUALS) == 0 ? 0 : 1;
+	    (processing_options & FIGPAR_BREAK_ON_SEMICOLON) == 0 ? 0 : 1;
 	case_sensitive =
-	    (processing_options & FIGPAR_CASE_SENSITIVE) == 0 ? 0 : 1;
+	    (processing_options & FIGPAR_CASE_SENSITIVE)     == 0 ? 0 : 1;
 	require_equals =
-	    (processing_options & FIGPAR_REQUIRE_EQUALS) == 0 ? 0 : 1;
+	    (processing_options & FIGPAR_REQUIRE_EQUALS)     == 0 ? 0 : 1;
 	strict_equals =
-	    (processing_options & FIGPAR_STRICT_EQUALS) == 0 ? 0 : 1;
+	    (processing_options & FIGPAR_STRICT_EQUALS)      == 0 ? 0 : 1;
 
 	/* Writing options from put_options */
 	backup   = (put_options & FIGPUT_SAVE_BACKUP)        == 0 ? 0 : 1;
@@ -547,10 +520,6 @@ put_config_end_statement:
 	} /* while (read) */
 
 put_config_eof:
-	/* Close read config (keep temp file open for additional writes) */
-	close(fd);
-	fd = -1;
-
 	/*
 	 * Write any remaining data trailing the last statement
 	 */
@@ -575,7 +544,11 @@ put_config_eof:
 		r = read(fd, statement, ssize - 1);
 		statement[ssize] = '\0';
 
-		/* XXX write statement to tmpfd XXX */
+		/* Write statement to tmpfd */
+                if (write(tmpfd, statement, ssize - 1) != r) {
+                        rv = -1;
+                        goto put_config_cleanup;
+                }
 	}
 
 	/* Loop through options we were told to put */
@@ -622,286 +595,3 @@ put_config_cleanup:
 
 	return (rv);
 }
-
-#if 0 /* REFERENCE MATERIAL */
-int
-emit_config(struct figpar_config options[static 1], const char *path,
-    const char *directive, const char *value, uint16_t par_options,
-    uint16_t put_options)
-{
-	int oldconfig;
-	ssize_t r = 1;
-	ssize_t dlen = strlen(directive);
-	ssize_t vlen = strlen(value);
-	ssize_t tlen = 0;
-	char buf[2] = {0};
-	char vbuf[STR_BUFSIZE]      = {0};
-	char tbuf[STR_BUFSIZE * 2]  = {0};
-	char bpath[PATH_MAX]        = {0};
-	enum figpar_cfgtype  optype = 0;
-	struct figpar_config config = {0}; /* In place configuration */
-	struct figpar_config option = {0}; /* Provided configuration */
-
-	if (value && !noval) {
-		if (quote)
-			snprintf(vbuf, vlen + 3, "\"%s\"", value);
-		else
-			snprintf(vbuf, vlen + 1, "%s", value);
-	}
-
-	/* Sanity check */
-	if (!directive)
-		errx(EX_USAGE, "Directive cannot be NULL");
-
-	for (size_t i = 0; options[i].directive; i++) {
-		/* Break if we have already filled the option */
-		if (option.directive)
-			break;
-		/* Continue if directive is not found */
-		if (strcmp(options[i].directive, directive))
-			continue;
-
-		option.type = options[i].type;
-		if (!(option.directive = strndup(options[i].directive,
-		    STR_BUFSIZE)))
-			err(EXIT_FAILURE, NULL);
-
-		switch (options[i].type) {
-		case FIGPAR_TYPE_NONE:
-			optype = FIGPAR_TYPE_NONE;
-			option.action = &setnone;
-			if (!noval) noval = 1;
-			if (value)
-				errx(1, "Directive does not accept a value");
-			break;
-		case FIGPAR_TYPE_BOOL:
-			/* XXX: Incomplete */
-			optype = FIGPAR_TYPE_BOOL;
-			option.action = &setbool;
-			if (noval && options[i].value.boolean == true)
-				errx(1, "Default value is already set true");
-			defcheck(boolean);
-			break;
-		case FIGPAR_TYPE_INT:
-			optype = FIGPAR_TYPE_INT;
-			option.action = &setnum;
-			if (!(option.value.num = strtol(value, 0, 10)))
-				err(EX_USAGE, "Value is not a number");
-			defcheck(num);
-			break;
-		case FIGPAR_TYPE_UINT:
-			optype = FIGPAR_TYPE_UINT;
-			option.action = &setnum;
-			if (!(option.value.u_num = strtol(value, 0, 10)))
-				err(EX_USAGE, "Value is not a number");
-			defcheck(u_num);
-			break;
-		case FIGPAR_TYPE_STR:
-			optype = FIGPAR_TYPE_STR;
-			option.action = &setstr;
-			if (!(option.value.str = strndup(value, STR_BUFSIZE)))
-				errx(1, "Could not use the value");
-			defcheck(str);
-			break;
-		/* XXX: Add FIGPAR_TYPE_STRARRAY case */
-		default:
-			errx(1, "Don't know how to handle type");
-		}
-	}
-
-	config = option;
-	parse_config(&config, path, NULL, par_options);
-
-	if ((found > 1) && nodup)
-		errx(EX_USAGE, "Resolve duplicates in the config file");
-
-	if (backup) {
-		if (snprintf(bpath, PATH_MAX, "%s.bak", rpath) < 0)
-			err(EXIT_FAILURE, "snprintf failed");
-		if (fig_backup(rpath, bpath))
-			err(EX_IOERR, "Could not backup the config file");
-	}
-
-	if ((oldconfig = open(rpath, O_RDWR)) < 0)
-		err(EX_IOERR, "Could not open the config file");
-
-	/*
-	 * Write to the temporary file up to the match line
-	 *
-	 * N.B. This means up to the EOF if either the
-	 * type is FIGPAR_TYPE_NONE or the directive is not
-	 * yet set (whether the default value's in use or
-	 * the option is not set at all.)
-	 */
-	unsigned l = 1;
-	while (r && l != fline) {
-		if ((r = read(oldconfig, buf, 1)) > 0)
-			if ((write(tmpfd, buf, 1)) != r)
-				err(EX_IOERR, "Write error");
-		if (r < 0)
-			err(EX_IOERR, "Read error");
-		if (*buf == '\n')
-			l++;
-	}
-
-	/* In case the directive stands without a value */
-	if (noval) {
-		if (found)
-			errx(EX_USAGE, "Value is already set");
-
-		if (write(tmpfd, option.directive, dlen) != dlen)
-			err(EX_USAGE, "Cannot write to the config file");
-
-		close(oldconfig);
-		close(tmpfd);
-
-		fig_move(tpath, rpath);
-
-		return EXIT_SUCCESS;
-	}
-
-	/* Format the directive/value string as requested */
-	if (par_options & FIGPAR_STRICT_EQUALS)
-		snprintf(tbuf, dlen + strlen(vbuf) + 2,
-		    "%s=%s", directive, vbuf);
-	else {
-		if (par_options & FIGPAR_REQUIRE_EQUALS)
-			snprintf(tbuf, dlen + strlen(vbuf) + 4,
-			    "%s = %s", directive, vbuf);
-		else
-			snprintf(tbuf, dlen + strlen(vbuf) + 2,
-			    "%s %s", directive, vbuf);
-	}
-	tlen = strlen(tbuf);
-
-	/*
-	 * If options is not set in the config file, append.
-	 * Otherwise, if FIGPAR_BREAK_ON_SEMICOLON is not set,
-	 * overwrite the line if found, or else find the position,
-	 * write the formatted directive/value, read from the
-	 * oldconfig up to either a semicolon or a '\n', and
-	 * finally copy the rest of the lines.
-	 */
-	if (!found) {
-		if (write(tmpfd, tbuf, strlen(tbuf)) != tlen)
-			err(EX_USAGE, "Write error");
-
-		close(oldconfig);
-		close(tmpfd);
-
-		fig_move(tpath, rpath);
-
-		return EXIT_SUCCESS;
-	}
-
-	/* Err if new value == old value from the config */
-	if (((optype == FIGPAR_TYPE_INT)  &&
-	     (option.value.num == config.value.num))     ||
-	    ((optype == FIGPAR_TYPE_UINT) &&
-	     (option.value.u_num == config.value.u_num)) ||
-	    ((optype == FIGPAR_TYPE_STR)  &&
-	     !strcmp(vbuf, config.value.str)))
-		errx(EX_USAGE,
-		    "new value == old value");
-
-	/*
-	 * XXX: Body of the following condition could be probably
-	 * written as a function.
-	 */
-	if (par_options & FIGPAR_BREAK_ON_SEMICOLON) {
-		ssize_t vlen = 0;
-		size_t counter = 0, idx = 0, pos = 0;
-		char lbuf[STR_BUFSIZE] = {0};
-		fig_node_t *head = NULL;
-		fig_node_t *last = NULL;
-		fig_node_t *tail = NULL;
-		fig_node_t *test = NULL;
-
-		while ((r = read(oldconfig, buf, 1)) > 0) {
-			if ((buf[0] == ';') || (buf[0] == '\n')) {
-				if (buf[0] == ';')
-					counter++;
-				if ((counter == 0 || counter == 1) && !head) {
-					test = tail = last = head =
-					    malloc(sizeof(fig_node_t) +
-					    strlen(lbuf) + 1);
-				}
-				else {
-					tail = malloc(sizeof(fig_node_t) +
-						strlen(lbuf) + 1);
-					last->next = tail;
-				}
-				snprintf(tail->val, strlen(lbuf) + 1, "%s",
-				    lbuf);
-				last = tail;
-				tail = tail->next;
-				memset(lbuf, 0, sizeof lbuf);
-				idx = 0;
-				if (buf[0] == '\n')
-					break;
-				continue;
-			}
-			lbuf[idx] = buf[0];
-			idx++;
-		}
-		last->next = NULL;
-
-		while (head && strlen(head->val) > 0) {
-			char *ptr = strstr(head->val, directive);
-			if (ptr) {
-				if (pos >= 1)
-					if ((write(tmpfd, " ", 1) != 1))
-						err(EX_USAGE, "Write error");
-				if (strncmp(ptr, directive, dlen + 1))
-					if ((write(tmpfd, tbuf,
-					    strlen(tbuf)) !=
-					    tlen))
-						err(EX_USAGE, "Write error");
-				if ((pos <= counter) &&
-				    ((write(tmpfd, ";", 1)) != 1))
-					err(EX_IOERR, "Write error");
-				head = head->next;
-				continue;
-			}
-			vlen = strlen(head->val);
-			if ((write(tmpfd, head->val, strlen(head->val))) !=
-			    vlen)
-				err(EX_IOERR, "Write error");
-			if ((write(tmpfd, ";", 1)) != 1)
-				err(EX_IOERR, "Write error");
-			head = head->next;
-			pos++;
-		}
-
-		/* Write '\n' if one is left */
-		if (r && (write(tmpfd, buf, 1)) != r)
-				err(EX_IOERR, "Write error");
-
-		/* Free the linked list */
-		fig_freelist(test);
-	} else {
-		if (write(tmpfd, tbuf, strlen(tbuf)) != tlen)
-			err(EX_USAGE, "Cannot write to the config file");
-		while ((r = read(oldconfig, buf, 1)) > 0)
-			if (buf[0] == '\n')
-				break;
-		if (r && write(tmpfd, buf, 1) != r)
-				err(EX_IOERR, "Write error");
-	}
-
-	/* Write the remaining lines from the old file */
-	while ((r = read(oldconfig, buf, 1)) > 0) {
-			if ((write(tmpfd, buf, 1)) != r)
-				err(EX_IOERR, "Write error");
-		if (r < 0)
-			err(EX_IOERR, "Read error");
-	}
-
-	close(oldconfig);
-	close(tmpfd);
-
-	fig_move(tpath, rpath);
-
-	return EXIT_SUCCESS;
-}
-#endif
