8812a4f53fb0075764dde2935627c1ececfca845
freebsdfrau <freebsdfrau@users.noreply.github.com>
2026-03-10 11:34:10 -0700

    libcmb: Fix cmb_private.h sentinel typo and update license
    Apply new standard license header documented in https://docs.freebsd.org/en/articles/license-guide/

--- depend/libcmb/cmb_private.h
+++ depend/libcmb/cmb_private.h
@@ -1,33 +1,11 @@
 /*-
- * Copyright (c) 2018 Devin Teske <dteske@FreeBSD.org>
- * All rights reserved.
+ * Copyright (c) 2018-2026 Devin Teske <dteske@FreeBSD.org>
  *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- * 
- * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
- * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
- * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
- * SUCH DAMAGE.
- *
- * $FreeBSD: head/usr.bin/dpv/dpv_util.h 274116 2014-11-04 23:46:01Z dteske $
+ * SPDX-License-Identifier: BSD-2-Clause
  */
 
-#ifndef _CMB_PRIVTE_H_
-#define _CMB_PRIVTE_H_
+#ifndef _CMB_PRIVATE_H_
+#define _CMB_PRIVATE_H_
 
 /* Limits */
 #define BUFSIZE_MAX		(2 * 1024 * 1024)
@@ -57,4 +35,4 @@
 #undef  MAX
 #define MAX(x,y) ((x) > (y) ? (x) : (y))
 
-#endif /* !_CMB_PRIVTE_H_ */
+#endif /* !_CMB_PRIVATE_H_ */
